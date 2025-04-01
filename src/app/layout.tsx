a7a2349c7e80738cdaf1d6b04f581aaf26c5e69c
Tech3 <Tech3@imperiorailing.com>
2025-04-01 14:37:27 +0530

    Dynamic Blogs Admin Panel Created Successfully

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,15 +23,6 @@ export default function RootLayout({
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
-    console.log("Entered Use Effect");
-    const fetchBlogs = async () => {
-      console.log("Entered Fetch Blogs");
-      const response = await fetch("/api/createConnection");
-      console.log("Response", response);
-      const data = await response.json();
-      console.log("Connection Status", data);
-    };
-    fetchBlogs();
     setMounted(true); // Set to true when the component is mounted on the client side
   }, []);
 
