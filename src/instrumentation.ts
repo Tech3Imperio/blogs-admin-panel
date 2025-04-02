ee863b76de82a7b75402e323e373b6b24c59fb90
Tech3 <Tech3@imperiorailing.com>
2025-04-02 14:26:13 +0530

    Dynamic Blogs Admin Panel Created Successfully

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,7 @@
 import dbConnect from "./lib/dbConnect";
 
 export async function register() {
+  console.log("In Instrumentation");
   if (process.env.NEXT_RUNTIME !== "nodejs") return;
   await dbConnect();
 }
